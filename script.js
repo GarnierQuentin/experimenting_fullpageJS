bb57c38e84339528f56b98805ffd70d730632402
Quentin Garnier <113943161+GarnierQuentin@users.noreply.github.com>
2024-01-09 11:21:27 +0100

    🎉 initializing project

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    new fullpage('#fullpage', {
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true
+    });
+});
